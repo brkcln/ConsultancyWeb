24919062e964300699f47196c6366f425cd06811
Burak <burakcelen17@gmail.com>
2021-07-02 01:07:20 -0700

    Add files via upload

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,23 +1,21 @@
 //Get the button:
 mybutton = document.getElementById("myBtn");
-header = document.getElementById("header-sticky");
-
+head = document.getElementById("header");
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
 	scrollFunction();
 };
-
 function scrollFunction() {
 	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
 		mybutton.style.display = "block";
-		header.classList.add("header-sticky");
+		head.classList.add("fix");
 	} else {
 		mybutton.style.display = "none";
-		header.classList.remove("header-sticky");
+		head.classList.remove("fix");
 	}
 }
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
 	document.body.scrollTop = 0; // For Safari
 	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+// When the user clicks on the button, scroll to the top of the document
