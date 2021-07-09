794e5296e87693e6f3a1024f8d45095ad6875012
Burak <burakcelen17@gmail.com>
2021-07-09 02:57:54 -0700

    Add files via upload

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,3 +1,5 @@
+//Anime js
+
 //Get the button:
 mybutton = document.getElementById("myBtn");
 head = document.getElementById("header");
@@ -18,4 +20,36 @@ function topFunction() {
 	document.body.scrollTop = 0; // For Safari
 	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
-// When the user clicks on the button, scroll to the top of the document
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav__list");
+
+hamburger.addEventListener("click", mobileMenu);
+function mobileMenu() {
+	hamburger.classList.toggle("active");
+	navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav__links");
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+function closeMenu() {
+	hamburger.classList.remove("active");
+	navMenu.classList.remove("active");
+}
+//slider
+var swiper = new Swiper(".mySwiper", {
+	slidesPerView: 3,
+	spaceBetween: 30,
+	autoplay: {
+		delay: 4500,
+	},
+	loop: true,
+	loopedSlides: 6,
+}); // When the user clicks on the button, scroll to the top of the document
+let hero = document.querySelectorAll(".anime-hero");
+anime({
+	targets: hero,
+	opacity: 1,
+	delay: 500,
+	translateX: -35,
+	ease: "linear",
+});
